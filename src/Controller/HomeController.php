3ef660e3205494b2fa321b81cec11bf04647ae7f
Luciana <sato.lu@gmail.com>
2018-03-22 21:15:45 -0300

    Fix route controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Flex\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class HomeController extends Controller
 {
